4fef3eac558075a3629bab633754dccd751bd124
Diego Gonzalez <diegogonzalez2511@gmail.com>
2022-11-03 11:13:32 -0600

    first commit

--- src/app/pages/alta-facturas/alta-facturas.component.ts
+++ src/app/pages/alta-facturas/alta-facturas.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CamaraService } from '../../services/camara.service';
 import { UsuarioService } from '../../services/usuario/usuario.service';
+import swal2 from 'sweetalert2';
+import { AltaSolicitudesService } from '../../services/altasolicitudes/altasolicitudes.service';
 
 @Component({
   selector: 'app-alta-facturas',
@@ -11,24 +13,33 @@ export class AltaFacturasComponent implements OnInit {
 
   tomada = false;
   foto;
+  idu = localStorage.getItem('id');
+  company = '';
 
   constructor(private camaraService:CamaraService,
-              private _usuarioService: UsuarioService) { }
+              private _usuarioService: UsuarioService,
+              private _solicitudesservice: AltaSolicitudesService) { }
 
   ngOnInit(): void {
     this.abrirCamara();
+
+    this._solicitudesservice.getCadenaProveedor(this.idu).subscribe( resp => {
+      if (resp.length > 0) {
+        this.company= resp[0].company_id;
+      }
+    });
   }
 
   abrirCamara(){
     this.foto = '';
     this.camaraService.abrirCamara();
+    
   }
 
   tomarFoto(){
     this.tomada = true;
 
     this.foto = this.camaraService.tomarFoto();//Esto regresa la base 64 de la foto
-    
     this.camaraService.cerrarCamara();
   }
 
@@ -38,11 +49,36 @@ export class AltaFacturasComponent implements OnInit {
 
   cancelar(){
     this.abrirCamara();
-
+    this.tomada = false;
   }
 
   subirFoto(){
-    console.log(this.foto);
+    if(this.company == '') {
+      swal2.fire('Atención', 'El usuario no está asignado a una compañía', 'info');
+      return;
+    }
+      let params = {};
+      let document_shot = {
+        document_id: '',
+        notes: '',
+        shot: this.foto,
+        company_id: this.company,
+        status: 'PENDIENTE'
+      }
+      params['document_shot'] = document_shot;
+      this.camaraService.subirFoto(params).subscribe(resp => {
+        swal2.fire('Éxito', 'Imagen enviada correctamente', 'success');
+        this.abrirCamara();
+        this.tomada = false;
+      }, err => {
+        let error = '';
+        err.error.errors.forEach(e => {
+          error += e;
+          error += '<br>';
+        });
+        swal2.fire('Atención', error, 'info');
+      })
+
   }
 
 }
--- src/app/services/camara.service.ts
+++ src/app/services/camara.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.prod';
+import { Usuario } from '../models/usuario.model';
+import { HttpClient } from '@angular/common/http';
+import { map, timeout } from 'rxjs/operators';
+// import 'rxjs/add/operator/map';
 
 @Injectable({
   providedIn: 'root'
@@ -7,17 +12,61 @@ export class CamaraService {
 
   videoNode;
   stream;
+  token: string;
+  usuario: Usuario;
 
-  constructor() {}
+  constructor(
+    public http: HttpClient
+  ) {
+    this.cargarStorage();
+  }
+
+  cargarStorage() {
+
+    if ( localStorage.getItem('token')) {
+      this.token = localStorage.getItem('token');
+      this.usuario = JSON.parse( localStorage.getItem('usuario') );
+    } else {
+      this.token = '';
+      this.usuario = null;
+    }
+
+  }
 
   abrirCamara(){
-    navigator.mediaDevices.getUserMedia({
-      audio: false,
-      video: {width: 300, height: 300}
-    }).then(stream => {
-      this.videoNode = document.getElementById('player');
-      this.videoNode.srcObject = stream;
-      this.stream = stream;
+    
+    navigator.mediaDevices.enumerateDevices().then(devices => {
+      let device = devices.find(d => d.kind == 'videoinput' && d.label.includes('back'));
+      let deviceId = '';
+      if(!device){
+        device = devices.find(d => d.kind == 'videoinput');
+      }
+      deviceId = device.deviceId;
+      let constraints = {
+        audio: false,
+        video: {
+          // width: 100%,
+          // height: 100%,
+          // width: {
+          //   min: 640,
+          //   ideal: 960,
+          //   max: 1280,
+          // },
+          // height: {
+          //   min: 360,
+          //   ideal: 540,
+          //   max: 720
+          // },
+          deviceId: {
+            exact: deviceId
+          }
+        }
+      }
+      navigator.mediaDevices.getUserMedia(constraints).then(stream => {
+        this.videoNode = document.getElementById('player');
+        this.videoNode.srcObject = stream;
+        this.stream = stream;
+    });
 
     })
   }
@@ -48,6 +97,20 @@ export class CamaraService {
 
     return foto;
 
+}
+
+subirFoto(params){
+  params.token = this.token;
+  params.secret_key = environment.SECRET_KEY;
+  // console.log(params);
+  console.log(params);
+  const url = `${environment.URL_SERVICIOS}/document_shots?`;
+
+  return this.http.post( url, params ).pipe(
+              map( (resp: any) => {
+                // console.log(resp);
+                return resp //this.crearArreglosimul(resp);
+              }));
 }
 
 
